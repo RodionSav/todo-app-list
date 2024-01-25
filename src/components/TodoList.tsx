77a252a826e9e8f93849ea56d59e931787eb48fb
Rodionsav <rodionsav@mate.academy>
2024-01-25 22:40:24 +0100

    todo-app-list clone

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {
+  CSSTransition,
+  TransitionGroup
+} from 'react-transition-group';
 import { Todo } from '../types/Todo';
 import { TodoItem } from './TodoItem';
 import { Status } from '../types/Status';
-import {
-  CSSTransition,
-  TransitionGroup } from 'react-transition-group';
 
 type Props = {
   todos: Todo[],
@@ -51,6 +52,5 @@ export const TodoList: React.FC<Props> = ({
         </CSSTransition>
       ))}
     </TransitionGroup>
-      
   );
 };
